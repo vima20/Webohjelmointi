a6afb062025531b761f8d2edab1f4bd4400e8019
vima20 <122276969+vima20@users.noreply.github.com>
2023-10-19 11:26:02 +0300

    Create scripts.js

---
+++ ssets/scripts.js
@@ -0,0 +1,21 @@
+alert("Tämä on minun portfolio!");
+
+    try {
+  // Yritä suorittaa jotain, joka saattaa aiheuttaa virheen
+  // Tässä voisi olla esimerkiksi epäonnistunut API-kutsu
+  fetch(https://html-css-js.com/)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('API-kutsu epäonnistui');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Käsittele data normaalisti
+    });
+} catch (error) {
+  // Jos virhe tapahtuu, seuraava lohko käsittelee sen
+  console.error('Virhe API-kutsussa:', error);
+  // Tee tarvittavat toimenpiteet virheen varalta
+}
+   
